e58ca1b4229783f80b5918ece725fa3422d3d438
jaseongKim <jaseong4281@gmail.com>
2022-08-13 22:16:44 +0900

    서버연결 / 상품 불러오기

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
+import Navi from "../components/Navi";
 import { PostList } from "../components/PostList";
 
 const Main = () => {
   const [category, setCartegory] = useState(0);
 
-  console.log(category);
-
   return (
     <Layout>
-      <Header category={category} setCartegory={setCartegory} />
+      <Stiky>
+        <Header />
+        <Navi setCartegory={setCartegory} />
+      </Stiky>
       <PostList category={category} />
     </Layout>
   );
 };
 
 export default Main;
+
+const Stiky = styled.section`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+`;
