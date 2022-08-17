ed81559a976e31f802f728e1333c3760aaf540aa
jaseongKim <jaseong4281@gmail.com>
2022-08-17 20:35:38 +0900

    no message

--- src/redux/modules/instance.js
+++ src/redux/modules/instance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  process.env.REACT_APP_API_URL,
+  baseURL:   process.env.REACT_APP_API_URL,
 });
 const token = localStorage.getItem("token");
-instance.defaults.headers.common["authorization"] = token
-  ? `${token}`
+instance.defaults.headers.common["authorization"] = token 
+  ? `${token}` 
   : null;
 export default instance;
